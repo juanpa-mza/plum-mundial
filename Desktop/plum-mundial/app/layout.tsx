cf2b2c4873ed371379f47e2d01915e5cf9d47131
juanpa-mza <67879342+juanpa-mza@users.noreply.github.com>
2026-06-06 13:02:29 -0300

    feat: Google Tag Manager

--- Desktop/plum-mundial/app/layout.tsx
+++ Desktop/plum-mundial/app/layout.tsx
@@ -26,7 +26,25 @@ export const viewport: Viewport = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es" className={`${nunito.variable} ${bebas.variable}`}>
+      <head>
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{ __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-KLQLHMH3');
+        `}} />
+      </head>
       <body className="font-body antialiased bg-plum-dark">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-KLQLHMH3"
+            height="0" width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         {children}
       </body>
     </html>
